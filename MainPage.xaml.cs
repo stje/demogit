79b69ad30bd85707a24b7cb7930eba1da3c465c1
stje <stje@easj.dk>
2014-10-06 12:33:33 +0200

    Kommentar tilføjet på knap

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace DemoGit
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           // dette er en prøve på en kommentar
+        }
     }
 }
